181eee5e64692f061410b097c081d9c74c567a3a
whoyoux <kamil@tecza.hub.pl>
2022-10-23 19:24:38 +0200

    Added files

---
+++ src/server/trpc/router/employee.ts
@@ -0,0 +1,13 @@
+import { router, protectedProcedure } from "../trpc";
+import { z } from "zod";
+
+export const employeeRouter = router({
+  get: protectedProcedure
+    .input(z.object({ id: z.string().nullish() }).nullish())
+    .query(({ input }) => {
+      return `${input?.id}`;
+    }),
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+});
